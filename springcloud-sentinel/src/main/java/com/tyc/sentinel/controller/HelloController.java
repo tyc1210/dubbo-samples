24f006437bc300bc04014a6da24e6ebad38d6cd5
tianyicheng <1573496757@qq.com>
2022-08-02 21:41:24 +0800

    '测试'

--- springcloud-sentinel/src/main/java/com/tyc/sentinel/controller/HelloController.java
+++ springcloud-sentinel/src/main/java/com/tyc/sentinel/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class HelloController {
 
@@ -15,5 +17,11 @@ public class HelloController {
     @GetMapping("clean/{id}")
     public String url(@PathVariable String id){
         return "hello " + id;
+    }
+
+    @GetMapping("test")
+    public String test() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(210);
+        return "test";
     }
 }
